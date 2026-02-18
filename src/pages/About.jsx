2db2f5a4f83f93ef78e99756b525b1bfa3e588bc
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2026-02-18 12:37:24 -0800

    Initial Commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <p>Our code monkey is working on the About section.. Stay tuned!</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Services.jsx
@@ -0,0 +1,10 @@
+function Services() {
+  return (
+    <div className="services">
+      <h1>Services</h1>
+      <p>Our code monkey is working on the Services section.. Stay tuned!</p>
+    </div>
+  );
+}
+
+export default Services;
